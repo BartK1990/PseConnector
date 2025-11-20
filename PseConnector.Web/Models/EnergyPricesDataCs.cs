ee4395c1f1d0e1119b885981914a88cc182c20d3
Bartosz Kuriata <kuriata.bartosz@gmail.com>
2025-11-20 20:46:20 +0100

    Added energy-prices

--- PseConnector.Web/Models/EnergyPricesDataCs.cs
+++ PseConnector.Web/Models/EnergyPricesDataCs.cs
@@ -7,7 +7,15 @@ public class EnergyPricesDataCs
 
 public class EnergyPricesDataCsValue
 {
-    public double? price { get; set; }
+    public double? cor_cost { get; set; }
+    public double? ceb_pp_cost { get; set; }
+    public double? ceb_sr_cost { get; set; }
+    public double? cen_cost { get; set; }
+    public double? csdac_pln { get; set; }
+    public double? sk_cost { get; set; }
+    public double? balance { get; set; }
+    public double? balance_power { get; set; }
+    public double? sk_cost_power { get; set; }
     public string dtime_utc { get; set; }
     public string business_date { get; set; }
 }
